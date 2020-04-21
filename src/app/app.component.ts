64e8e15bf2d708985766dcdad340245e25856cda
Jamesson Faria <jamessonjr@gmail.com>
2020-04-21 18:39:23 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { Student } from './student/student.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jedi-academy';
+
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Han Solo', isJedi: false},
+    {name: 'Leia', isJedi: false}  
+  ]
 }
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Student } from './student.model';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  @Input() student: Student
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
